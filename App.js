69277857b8732da9640f74a5c631360dec56368c
Praveen Jayamohan <pjayamoh@netapp.com>
2014-04-24 20:25:11 -0400

    cleaned up code

--- App.js
+++ App.js
@@ -1,30 +1,40 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc2/doc/">App SDK 2.0rc2 Docs</a>'},
+
+    // entry point of application
     launch: function() {
         console.log("My first app .... hoo hoo!");
-        Ext.create('Rally.data.wsapi.Store', {
+        this._loadData();
+    },
+
+    // get data from rally
+    _loadData: function() {
+        var myStore = Ext.create('Rally.data.wsapi.Store', {
             model: 'User Story',
             autoLoad: true,
             listeners: {
                 load: function(myStore, myData, success) {
                     //process data
                     console.log("Got data!", myStore, myData, success);
-
-                    var myGrid = Ext.create('Rally.ui.grid.Grid', {
-                        store: myStore,
-                        columnCfgs: [
-                            'FormattedID', 'Name', 'ScheduleState'
-                        ]
-                    });
-                    console.log("My grid!", myGrid);
-                    this.add(myGrid);
-                    console.log("what is this ", this);
+                    this._loadGrid(myStore);
                 },
                 scope: this
             },
             fetch: ['FormattedID', 'Name', 'ScheduleState']
         });
+    },
+
+    // create and show a list of stories
+    _loadGrid: function(myStoryStore) {
+        var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            store: myStoryStore,
+            columnCfgs: [
+                'FormattedID', 'Name', 'ScheduleState'
+            ]
+        });
+        console.log("My grid!", myGrid);
+        this.add(myGrid);
+        console.log("what is this ", this);
     }
 });
